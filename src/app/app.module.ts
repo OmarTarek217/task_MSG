5a3508bb0ee0778029d71f70131fbca1a101d537
Omar Tarek <omar20051210@gmail.com>
2024-09-20 21:22:22 +0300

    added clothes pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainpartComponent } from './mainpart/mainpart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainpartComponent
   ],
   imports: [
     BrowserModule,
