59858c66965204b72ad3f8a4828ce022fa09ea6e
Omar Tarek <omar20051210@gmail.com>
2024-09-23 20:21:37 +0300

    added routing & products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpartComponent } from 'src/app/mainpart/mainpart.component';
+import { ProductsComponent } from 'src/app/products/products.component';
+// import { DefaultProductsComponent } from './Products/default-products/default-products.component';
+// import { MenWearComponent } from './Products/men-wear/men-wear.component';
+// import { WomenWearComponent } from './Products/women-wear/women-wear.component';
+// import { KidWearComponent } from './Products/kid-wear/kid-wear.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: "home", component: MainpartComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "products", component: ProductsComponent,
+    // children: [
+    //   { path: "", component: DefaultProductsComponent },
+    //   { path: "menWear", component: MenWearComponent },
+    //   { path: "womenWear", component: WomenWearComponent },
+    //   { path: "kidWear", component: KidWearComponent }
+    // ]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+})  
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainpartComponent } from './mainpart/mainpart.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MainpartComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
