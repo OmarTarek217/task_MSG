901871b067e301b108bedec20ff1259170875aa2
Omar Tarek <omar20051210@gmail.com>
2024-09-26 20:29:02 +0300

    Redesigned men-wear page & added ngx-owl-carousel

--- src/app/men-wear/men-wear.component.ts
+++ src/app/men-wear/men-wear.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/product.service';
+import * as ngxOwlCarouselO from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-men-wear',
   templateUrl: './men-wear.component.html',
   styleUrls: ['./men-wear.component.css']
 })
-export class MenWearComponent {
-
+export class MenWearComponent implements OnInit {
+constructor(private service:ProductsService){}
+products:any
+ngOnInit(): void {
+  this.service.productsCategory().subscribe({
+    next:(response)=>{
+      console.log(response.data);
+      this.products=response.data
+    }
+  })
+}
+customOptions: ngxOwlCarouselO.OwlOptions = {
+  loop: true,
+  mouseDrag: true,
+  touchDrag: true,
+  pullDrag: false,
+  dots: false,
+  navSpeed: 700,
+  navText: ['<i class="fas fa-chevron-circle-left"></i>', '<i class="fas fa-chevron-circle-right"></i>'],
+  responsive: {
+    0: {
+      items: 1
+    },
+    400: {
+      items: 2
+    },
+    740: {
+      items: 3
+    },
+    940: {
+      items: 4
+    }
+  },
+  nav: true
+}
 }
